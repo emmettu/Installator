15b6da3aada655b308d4158ebfdfb89ddfae30e7
Emmett Underhill <eunderhi@redhat.com>
2016-03-17 14:34:01 -0400

    remove Controller error handling in GUIComponent
    This is because this is handled by the validator api.

--- src/main/java/views/ui/UIComponent.java
+++ src/main/java/views/ui/UIComponent.java
@@ -1,8 +1,6 @@
 package views.ui;
 
 import controllers.Controller;
-import controllers.exceptions.ControllerFailException;
-import controllers.exceptions.ControllerWarnException;
 import views.View;
 
 import java.util.ArrayList;
@@ -17,26 +15,10 @@ public abstract class UIComponent implements View {
     List<Controller> controllers = new ArrayList<>();
 
     public void notifyControllers() {
-        try {
-            checkAllControllers();
-        }
-        catch (ControllerFailException e) {
-            onControllerFail(e);
-        }
-    }
-
-    private void checkAllControllers() throws ControllerFailException {
         for(Controller c : controllers) {
-            performAction(c);
+            c.performAction();
         }
     }
-
-    private void performAction(Controller c) throws ControllerFailException {
-        c.performAction();
-    }
-
-    protected abstract void onControllerFail(ControllerFailException e);
-    protected abstract void onControllerWarn(ControllerWarnException e);
 
     public void addController(Controller controller) {
         controllers.add(controller);
--- src/main/java/views/ui/gui/GUIComponent.java
+++ src/main/java/views/ui/gui/GUIComponent.java
@@ -1,9 +1,5 @@
 package views.ui.gui;
 
-import controllers.exceptions.ControllerFailException;
-import controllers.exceptions.ControllerWarnException;
-import views.lookandfeel.UiResources;
-import views.notification.ToastMessage;
 import views.ui.UIComponent;
 
 import javax.swing.*;
@@ -46,20 +42,6 @@ public abstract class GUIComponent extends UIComponent {
 
     public void setBorder(Border border) {
         jComponent.setBorder(border);
-    }
-
-    @Override
-    protected void onControllerFail(ControllerFailException e) {
-        jComponent.setToolTipText(e.getMessage());
-        jComponent.setBackground(UiResources.validationFail);
-        new ToastMessage(e.getMessage(), 4000, jComponent);
-    }
-
-    @Override
-    protected void onControllerWarn(ControllerWarnException e) {
-        jComponent.setToolTipText(e.getMessage());
-        jComponent.setBackground(UiResources.validationWarn);
-        new ToastMessage(e.getMessage(), 4000, jComponent);
     }
 
 }
