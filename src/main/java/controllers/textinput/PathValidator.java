00c083109bdeb51719a6bdd1e78dcfaeffd27453
Emmett Underhill <eunderhi@redhat.com>
2016-03-18 15:35:14 -0400

    Change PathValidator messages

--- src/main/java/controllers/textinput/PathValidator.java
+++ src/main/java/controllers/textinput/PathValidator.java
@@ -8,6 +8,10 @@ import java.io.File;
 
 /**
  * Created by eunderhi on 19/01/16.
+ * Given a string, checks if it is a valid path:
+ * 1. Exists
+ * 2. Is a directory
+ * 3. Is write-able
  */
 public class PathValidator implements Validator<String> {
 
@@ -17,13 +21,13 @@ public class PathValidator implements Validator<String> {
         File file = new File(pathName);
 
         if (!file.exists()) {
-            throw new ControllerFailException("Path must exist");
+            throw new ControllerFailException("This path does not exist.");
         }
         else if (!file.isDirectory()) {
-            throw new ControllerFailException("Path must be a directory.");
+            throw new ControllerFailException("This path must be a directory.");
         }
         else if (!file.canWrite()) {
-            throw new ControllerFailException("Path must be write-able.");
+            throw new ControllerFailException("The path must be write-able.");
         }
     }
 
